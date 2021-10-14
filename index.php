8e0333b6ae44c5340f2b1c8164c2e3e74df90ab3
Michele <micheleierniero@gmail.com>
2021-10-14 12:43:17 +0200

    snack 8, stampa pubblicità

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         #Snack_Cinque {margin-bottom: 50px;}
         #Snack_Sei .grigio {border: 5px solid gray;}
         #Snack_Sei .verde {border: 5px solid green;}
-        #Snack_Sette {margin-top: 50px;}
+        #Snack_Sette {margin-top: 50px; margin-bottom: 50px;}
     </style>
 </head>
 <body>
@@ -312,6 +312,61 @@
 
         </div>
         <!-- / SNACK 7  -->
+
+        <!-- SNACK 8  -->
+        <div id="snack_otto">SNACK OTTO
+
+            <?php
+
+                $ads = [
+                    [
+                        'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
+                        'link' => 'https://www.google.com',
+                        'is_active' => true,
+                    ],
+                    [
+                        'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
+                        'link' => 'https://www.facebook.com',
+                        'is_active' => true,
+                    ],
+                    [
+                        'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
+                        'link' => 'https://codepen.io',
+                        'is_active' => false,
+                    ],
+                    [
+                        'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
+                        'link' => 'https://laravel.com',
+                        'is_active' => false,
+                    ],
+                    [
+                        'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
+                        'link' => 'https://www.php.net',
+                        'is_active' => true,
+                    ],
+
+                ];
+
+            ?>
+
+            <div>
+
+                <?php foreach($ads as $ad) { ?>
+
+                    
+                    <?php if ($ad["is_active"] === true) { ?>
+
+                        <img src="<?php echo $ad['image_path'] ?>" alt="">
+
+                    <?php } ?>
+                    
+
+                <?php } ?>
+
+            </div>
+
+        </div>
+        <!-- / SNACK 8  -->
 
     </div>
 </body>
